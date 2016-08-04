ede408ec9f4d0762c01bf51f2e56b2db5df580b2
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-08-04 20:49:51 +0200

    Added checkstyle

--- sample/src/main/java/es/voghdev/progressbuttonview/MainActivity.java
+++ sample/src/main/java/es/voghdev/progressbuttonview/MainActivity.java
@@ -24,6 +24,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
