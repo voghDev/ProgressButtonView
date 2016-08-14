cb135088940cd1be406d6d0269a391b463bdf0f9
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-08-14 15:53:23 +0200

    Added first UI test. Passing

---
+++ sample/src/androidTest/java/es/voghdev/progressbuttonview/MainActivityTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 Olmo Gallegos Hernández
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.voghdev.progressbuttonview;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import es.voghdev.progressbuttonview.sample.MainActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest  {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class, true, false);
+
+    @Test
+    public void showsSamplesTitle() {
+        startActivity();
+
+        onView(withText("Hello!")).check(matches(isDisplayed()));
+    }
+
+    private MainActivity startActivity() {
+        return activityRule.launchActivity(null);
+    }
+}
