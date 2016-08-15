0271b136f159586b68879df50769f672406e1126
Olmo Gallegos <vendofiatpunto@hotmail.com>
2016-08-15 12:49:57 +0200

    attempt to show loading state

--- sample/src/androidTest/java/es/voghdev/progressbuttonview/MainActivityTest.java
+++ sample/src/androidTest/java/es/voghdev/progressbuttonview/MainActivityTest.java
@@ -18,22 +18,29 @@ package es.voghdev.progressbuttonview;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import es.voghdev.progressbuttonview.sample.MainActivity;
+import es.voghdev.progressbuttonview.sample.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MainActivityTest  {
+public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> activityRule =
             new ActivityTestRule<>(MainActivity.class, true, false);
@@ -45,7 +52,33 @@ public class MainActivityTest  {
         onView(withText("Hello!")).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void shouldBeLoadingAfterProgressButtonViewIsClicked() throws Exception {
+        startActivity();
+
+        onView(withId(R.id.progressButtonView)).perform(click());
+        onView(withId(R.id.progress_button_progressBar)).check(matches(isDisplayed()));
+
+//        onView(withId(R.id.progressButtonView)).check(matches(isLoading()));
+//        onView(withId(R.id.progressButtonView)).check(matches(new ProgressButtonViewLoadingMatcher()));
+    }
+
     private MainActivity startActivity() {
         return activityRule.launchActivity(null);
+    }
+
+    public static Matcher<View> isLoading() {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("is showing loading state");
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ProgressButtonView progressButtonView = (ProgressButtonView) view;
+                return progressButtonView.isLoading();
+            }
+        };
     }
 }
