4cdd8fce7588c2ea16a9c8ff1af8b80ec79ba1e6
Olmo Gallegos <vendofiatpunto@hotmail.com>
2017-01-28 20:56:30 +0100

    Issue reproduced. Test failing

---
+++ sample/src/androidTest/java/es/voghdev/progressbuttonview/SetTextIssueTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Olmo Gallegos Hernández.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package es.voghdev.progressbuttonview;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import es.voghdev.progressbuttonview.sample.SetTextIssueActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SetTextIssueTest {
+    @Rule
+    public ActivityTestRule<SetTextIssueActivity> activityRule =
+            new ActivityTestRule<>(SetTextIssueActivity.class, true, false);
+
+    @Test
+    public void shouldShowTheCorrectTextAfterShowingAndHidingLoading() throws Exception {
+        startActivity();
+
+        onView(withText("Text Before")).check(matches(isDisplayed()));
+        onView(withText("Text Before")).perform(click());
+        onView(withText("Text Before")).check(matches(not(isDisplayed())));
+        onView(withText("Text After")).check(matches(isDisplayed()));
+    }
+
+    private SetTextIssueActivity startActivity() {
+        return activityRule.launchActivity(null);
+    }
+}
---
+++ sample/src/main/java/es/voghdev/progressbuttonview/sample/SetTextIssueActivity.java
@@ -0,0 +1,34 @@
+package es.voghdev.progressbuttonview.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import es.voghdev.progressbuttonview.ProgressButtonView;
+
+public class SetTextIssueActivity extends AppCompatActivity {
+    ProgressButtonView progressButtonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        progressButtonView = (ProgressButtonView) findViewById(R.id.progressButtonView);
+        progressButtonView.setText("Text Before");
+
+        progressButtonView.showLoading();
+        progressButtonView.setText("Text After");
+        progressButtonView.hideLoading();
+
+        progressButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressButtonView.showLoading();
+                progressButtonView.setText("Text After");
+                progressButtonView.hideLoading();
+            }
+        });
+    }
+}
